6807dfdacf26946a9363ae714dfdab98812bb348
kkanjjak <112145268+kkanjjak@users.noreply.github.com>
2023-08-17 12:59:58 +0900

    Add files via upload

--- Create.js
+++ Create.js
@@ -14,17 +14,41 @@ function Create(props) {
             props.onCreate(keyword, context, author);
           }}
         >
-          <div>
-            <input name="keyword" type="text" placeholder="keyword" />
-          </div>
-          <div>
-            <textarea name="context" type="text" placeholder="context" />
-          </div>
-          <div>
-            <input name="author" type="text" placeholder="author" />
-          </div>
-          <div>
-            <input type="submit" value="수정하기" />
+          <div className="createGrid">
+            <input
+              className="createDetail"
+              name="keyword"
+              type="text"
+              placeholder="keyword"
+              required
+            />
+            <input
+              className="createDetail"
+              name="author"
+              type="text"
+              placeholder="author"
+              required
+            />
+            <textarea
+              name="context"
+              type="text"
+              placeholder="내용을 작성하여 주십시오"
+              required
+            />
+            <button
+              className="createBtn"
+              type="submit"
+              value="추가하기"
+              style={{
+                border: "2px solid tomato",
+                borderRadius: 5,
+                color: "tomato",
+                fontWeight: "bold",
+                backgroundColor: "white",
+              }}
+            >
+              추가하기
+            </button>
           </div>
         </form>
       </div>
